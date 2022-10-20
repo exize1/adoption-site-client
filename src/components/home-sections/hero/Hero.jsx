921f7a85d8ea6da1b73130efbb3f02022a955bfc
exize1 <amio121212@gmail.com>
2022-10-20 11:14:29 +0300

    fix-hero

--- src/components/home-sections/hero/Hero.jsx
+++ src/components/home-sections/hero/Hero.jsx
@@ -5,16 +5,19 @@ import backgroundvideo from './dogvideobg2.mp4'
 const Hero = () => {
     return(
         <div className="hero-container">
-            <video autoPlay loop muted id="backgroundvideo">
+            <div className="home-page-hero-container">
+                <div className="home-page-hero"/>
+                <div className="hero-text">
+                    <h2 className="hero-sub-title">עשרות כלבים מחכים לבית</h2>
+                    <h1 className="hero-title">בואו לאמץ חבר/ה</h1>
+                    <div className="hero-btn-container">
+                        <button className="hero-btn">אמצו עכשיו</button>
+                    </div>
+                </div>
+            </div>
+            <video autoPlay loop muted id="background-video">
                 <source src={backgroundvideo}  type="video/mp4"/>
             </video>
-            <div className="homepagehero"></div>
-            <div className="herotext">
-            <h2 className="hero-sub-title">עשרות כלבים מחכים לבית</h2>
-            <h1 className="hero-title">בואו לאמץ חבר/ה</h1>
-            <div className="hero-btn-container">
-                <button className="hero-btn">אמצו עכשיו</button>
-            </div></div>
         </div>
     )
 }
